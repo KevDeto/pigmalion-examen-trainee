2ff7fbffc538881b0e9cabbb5f4bffb89f00cee1
Kevin <104948198+KevDeto@users.noreply.github.com>
2025-06-15 05:35:14 -0300

    Solución para la problemática del exámen

--- pigmalion_examen_trainee/src/pigmalion/soluciones/Main.java
+++ pigmalion_examen_trainee/src/pigmalion/soluciones/Main.java
@@ -7,10 +7,10 @@ public class Main {
 		int[] nums1 = {1,4,3,9};
 		int[] nums2 = {1,2,4,4};
 		
-		System.out.println(recursos_infinitos.sumaDeDosElementosEsIgualANumero(nums1, 0));
+		System.out.println(recursos_infinitos.sumaDeDosElementosEsIgualANumero(nums1, 8));
 		System.out.println(recursos_infinitos.sumaDeDosElementosEsIgualANumero(nums2, 8));
 		
-		System.out.println(recursos_limitados.sumaDeDosElementosEsIgualANumero(nums1, 0));
+		System.out.println(recursos_limitados.sumaDeDosElementosEsIgualANumero(nums1, 8));
 		System.out.println(recursos_limitados.sumaDeDosElementosEsIgualANumero(nums2, 8));
 	}
 }
